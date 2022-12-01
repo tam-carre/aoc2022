f946839de08bae8400c118d5ac5cfb714114f2a7
Tam CARRE <kokone@takat.su>
2022-12-01 13:42:20 +0100

    day1: add doc comments + move input file to new folder

--- src/Day01.hs
+++ src/Day01.hs
@@ -4,14 +4,16 @@ import Data.List (maximum)
 import Data.List.Extra (takeEnd)
 import Data.Text qualified as T
 
+-- | Elf carrying the most calories
 day01A ∷ IO Int
 day01A = maximum . toIndividualCalCounts <$> getInput
 
+-- | Sum of carried calories by the 3 elves with the most calories
 day01B ∷ IO Int
 day01B = sum . takeEnd 3 . sort . toIndividualCalCounts <$> getInput
 
 getInput ∷ IO Text
-getInput = decodeUtf8 <$> readFileBS "./src/Day01AInput.txt"
+getInput = decodeUtf8 <$> readFileBS "./inputs/Day01AInput.txt"
 
 toIndividualCalCounts ∷ Text → [Int]
 toIndividualCalCounts = map sumCals . T.splitOn "\n\n" where
