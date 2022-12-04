24a018b8a33d8496e9a428952af65f19f113cf13
Tam CARRE <kokone@takat.su>
2022-12-04 10:30:22 +0100

    refactor day4

--- src/Day04.hs
+++ src/Day04.hs
@@ -6,17 +6,15 @@ import Relude.Unsafe   (read)
 
 day04A ∷ IO Int
 day04A = length . filter rangeFullyContainsOther <$> loadElfPairs where
-  rangeFullyContainsOther (a,b)  = a `contains` b ∨ b `contains` a
-  contains (loA, hiA) (loB, hiB) = [loB..hiB] `isInfixOf` [loA..hiA]
+  rangeFullyContainsOther (a,b) = a `isInfixOf` b ∨ b `isInfixOf` a
 
 day04B ∷ IO Int
 day04B = length . filter rangesOverlap <$> loadElfPairs where
-  rangesOverlap (a,b) = a `overlaps` b
-  overlaps (loA, hiA) (loB, hiB) = not . null $ [loA..hiA] `intersect` [loB..hiB]
+  rangesOverlap (a,b) = not . null $ a `intersect` b
 
-type Range = (Int, Int)
+type Range = [Int]
 
 loadElfPairs ∷ IO [(Range, Range)]
 loadElfPairs = parseElfPairs <$> readFile "./inputs/Day04.txt" where
-  parseElfPairs   = map (parsePair "," . parsePair "-" $ read) . strLines
-  parsePair sep f = (\[a,b] → (a,b)) . map f . splitOn sep
+  parseElfPairs = map ((\[a,b] → (a,b)) . map parseRange . splitOn ",") . strLines
+  parseRange    = (\[lo,hi] → [lo..hi]) . map read . splitOn "-"
